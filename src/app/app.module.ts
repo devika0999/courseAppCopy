8c550b50c0ad23fde08c05276ff43b0036120358
devika0999 <krishnadevika445@gmail.com>
2022-10-11 12:36:32 +0530

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { ViewcourseComponent } from './viewcourse/viewcourse.component';
 import { RouterModule, Routes } from '@angular/router';
+import {   HttpClientModule} from '@angular/common/http'
 
 const appRoutes:Routes=[
   {
@@ -26,7 +27,8 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
